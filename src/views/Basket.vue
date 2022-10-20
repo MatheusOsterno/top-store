e94aa218fdf33a0bc294fcc0a12097649aab03fb
MatheusOsterno <matheusosternodev@gmail.com>
2022-10-20 17:49:58 -0300

    projeto finalizado v1.0

--- src/views/Basket.vue
+++ src/views/Basket.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="basket">
     <div class="items">
+      
+      <template v-if="productsInBag.length">
 
-      <div class="item">
-        <div class="remove">Remover Produto</div>
-        <div class="photo"><img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt=""></div>
-        <div class="description">Mens Casual Premium Slim Fit T-Shirts </div>
-        <div class="price">
-          <span class="quantity-area">
-            <button disabled="">-</button>
-            <span class="quantity">1</span>
-            <button>+</button>
-          </span>
-          <span class="amount">R$ 22.30</span>
+        <div v-for="(product, index) in productsInBag" :key="index" class="item">
+          <div class="remove" @click="this.$store.dispatch('removeFromBag', product.id)">Remover Produto</div>
+          <div class="photo">
+            <img :src="product.image" alt="">
+          </div>
+          <div class="description">
+              {{product.title}}
+          </div>
+          <div class="price">
+            <span class="quantity-area">
+              <button :disabled="product.quantity<=1" @click="product.quantity--">-</button>
+              <span class="quantity">{{product.quantity}}</span>
+              <button @click="product.quantity++">+</button>
+            </span>
+            <span class="amount">R$ {{ (product.price * product.quantity).toFixed(2) }}</span>
+          </div>
         </div>
-      </div>
-      <div class="grand-total"> Total do pedido: R$ 22.30</div>
+        <div class="grand-total"> Total do pedido: R$ {{orderTotal()}}</div>
+
+      </template>
+
+      <template v-else>
+        <h4>Não há items no carrinho ainda</h4>
+      </template>
 
     </div>
   </div>
@@ -23,13 +35,23 @@
 
 <script>
 
+import { mapState } from 'vuex'
+
 export default {
   name: 'Basket',
 
   methods: {
-   
+    orderTotal() {
+      var total = 0;
+      this.productsInBag.forEach(item => {
+        total += item.price * item.quantity;
+      });
+      return total.toFixed(2);
+    }
   },
- 
+  computed: mapState([
+    'productsInBag' 
+  ]),
 }
 </script>
 
